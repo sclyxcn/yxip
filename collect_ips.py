df104369999911e16a2ef4c59abda4620b5220af
sclyxcn <sclyx@kingdee.ltd>
2025-07-21 11:19:40 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -11,38 +11,35 @@ urls = ['https://api.uouin.com/cloudflare.html',
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
-# 检查ip.txt文件是否存在,如果存在则删除它
-if os.path.exists('ip.txt'):
-    os.remove('ip.txt')
-
 # 创建一个文件来存储IP地址
-with open('ip.txt', 'w') as file:
-    for url in urls:
-        # 发送HTTP请求获取网页内容
-        response = requests.get(url)
-        
-        # 使用BeautifulSoup解析HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://api.uouin.com/cloudflare.html':
-            elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
-            elements = soup.find_all('tr')
-        else:
-            elements = soup.find_all('li')
-        
-        ip_set = set()
-        # 遍历所有元素,查找IP地址
-        for element in elements:
-            element_text = element.get_text()
-            ip_matches = re.findall(ip_pattern, element_text)
-            for ip in ip_matches:
-                ip_set.add(ip)
-        
-        ip_set = sorted(ip_set, key=lambda x: (int(x.split('.')[0]), int(x.split('.')[1]), int(x.split('.')[2])))                    
+ip_set = set()
+for url in urls:
+    # 发送HTTP请求获取网页内容
+    response = requests.get(url)        
+    # 使用BeautifulSoup解析HTML
+    soup = BeautifulSoup(response.text, 'html.parser')        
+    # 根据网站的不同结构找到包含IP地址的元素
+    if url == 'https://api.uouin.com/cloudflare.html':
+        elements = soup.find_all('tr')
+    elif url == 'https://ip.164746.xyz':
+        elements = soup.find_all('tr')
+    else:
+        elements = soup.find_all('li')      
+    # 遍历所有元素,查找IP地址
+    for element in elements:
+        element_text = element.get_text()
+        ip_matches = re.findall(ip_pattern, element_text)
+        for ip in ip_matches:
+            ip_set.add(ip)
+
+# 检查ip.txt文件是否存在,如果存在则删除它
+if os.path.exists('ip.txt') and len(ip_set)>0:
+    os.remove('ip.txt')
+    
+with open('ip.txt', 'w') as file:      
+    ip_set = sorted(ip_set, key=lambda x: (int(x.split('.')[0]), int(x.split('.')[1]), int(x.split('.')[2])))                    
         # 如果找到IP地址,则写入文件
-        for ip in ip_set:
-            file.write(ip + '\n')
+    for ip in ip_set:
+        file.write(ip + '\n')
 
 print('IP地址已保存到ip.txt文件中。')
